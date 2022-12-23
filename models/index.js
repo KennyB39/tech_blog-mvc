bd83a9b594210301dd7468d04412814333908b10
kennyb39 <chefken360@gmail.com>
2022-12-23 06:26:51 -0800

    User.js

---
+++ models/index.js
@@ -0,0 +1,28 @@
+const User = require('./User');
+const Blog = require('./Blog');
+const Comment = require('./Comment');
+
+User.hasMany(Blog, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Blog.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Blog.belongsTo(User, {
+    foreignKey: 'blog_id',
+});
+
+Comment.belongsTo(Blog, {
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+});
+
+Blog.hasMany(Comment, {
+    foreignKey: 'blog_id',
+    onDelete: 'CASCADE'
+});
+
+module.exports = {User, Blog, Comment};
